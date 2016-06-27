858d050072bb919ed34ff1aebfea9c0fcdd56f60
421p <andrew.bakal@gmail.com>
2016-06-28 02:52:30 +0300

    simplify serializer

--- frontend/Model/Message/MessageSerializer.cs
+++ frontend/Model/Message/MessageSerializer.cs
@@ -11,19 +11,7 @@ namespace AllyTalksClient.Model.Message {
 
         public static Message DeserializeMessage(string data)
         {
-            var json = JObject.Parse(data);
-            var type = (string) json["type"];
-
-            switch (type) {
-                case MessageType.Message:
-                   return JsonConvert.DeserializeObject<Message>(data);
-                case MessageType.Error:
-                    return JsonConvert.DeserializeObject<Message>(data);
-                case MessageType.Auth:
-                    return  JsonConvert.DeserializeObject<Message>(data);
-                default:
-                    throw new Exception("Can not handle this kind of message yet.");
-            }
+            return JsonConvert.DeserializeObject<Message>(data);
         }
     }
 }
